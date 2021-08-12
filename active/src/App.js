bea60d7881488f8f8892528a6c8185246ab8f0fd
Ashley Burns <ashleyburns@ashleys-mini.home>
2021-08-11 20:42:51 -0400

    mvp done will come back to- style- add dropdown menu to pick type of activity

---
+++ active/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Bored from './components/Bored'
+
+function App() {
+  return (
+    <div className="App">
+      <Bored />
+    </div>
+  );
+}
+
+export default App;
